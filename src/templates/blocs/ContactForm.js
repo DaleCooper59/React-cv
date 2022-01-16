e4ea403a3525d1b2f0b2984f3fc4ecc45786ad10
DaleCooper59 <duvinagechristopher1@gmail.com>
2022-01-16 15:30:42 +0100

    'update'

--- src/templates/blocs/ContactForm.js
+++ src/templates/blocs/ContactForm.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
     return (
-        <div class="sub2_footer block_footer3">
-        <form class="contact-form" action="" method="post">
-        <h5>En construction ...</h5>
+        <div className="sub2_footer block_footer3">
+        <form className="contact-form" action="" method="post">
+        
         <label for="name">Nom et prénom</label>
-        <input type="text" id="name" name="name" class="text-box" pattern="^[A-Za-z ,.'-]+$" minlength="4" maxlength="80" placeholder="Votre nom et prénom" required/>
+        <input type="text" id="name" name="name" className="text-box" pattern="^[A-Za-z ,.'-]+$" minlength="4" maxlength="80" placeholder="..." required/>
 
         <label for="mail">Mail</label>
-        <input type="email" id="mail" name="email" class="text-box" placeholder="Votre Email" required/>
+        <input type="email" id="mail" name="email" className="text-box" placeholder="...@..." required/>
        
-        <label for="textarea">Message</label>
-        <textarea name="message" id="textarea" rows="5" minlength="15" placeholder="Votre message" required></textarea>
-
-        <input type="submit" name="submit" class="btn-form" value="Envoyer"/>
+        <label for="textarea">Votre message</label>
+        <textarea name="message" id="textarea" rows="5" minlength="15" required></textarea>
+        
+        <input type="button" name="submit" className="btn-form" value="Envoyer"/>
+        <div className="form-message"></div>
         </form>
     </div>
     );
